8c005a08843c26c1c3a0f25d1660727480fd64ff
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-07-28 14:28:28 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startBtn.addEventListener("click", handleStartClick)
+let timerId = null;
+
+function handleStartClick() {
+   timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+   }, 1000);
+    startBtn.disabled = true;
+}
+
+stopBtn.addEventListener("click", handleStopClick)
+
+function handleStopClick() {
+    body.style.backgroundColor = clearInterval(timerId);
+    startBtn.disabled = false;
+}
+
 
