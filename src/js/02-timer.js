7faafd992a523b2822c1a8b82ee620bcb8037266
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-08-01 01:51:19 +0300

    update task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,6 +5,11 @@ const pickerInput = document.querySelector("#datetime-picker");
 const startBtn = document.querySelector("[data-start]");
 startBtn.disabled = true;
 
+let chosenDate = 0;
+let todaysDate = new Date();
+let timerId = null;
+
+
 const daysRef = document.querySelector("[data-days]");
 const hoursRef = document.querySelector("[data-hours]");
 const minutesRef = document.querySelector("[data-minutes]");
@@ -13,43 +18,61 @@ const secondsRef = document.querySelector("[data-seconds]");
 
 const options = {
     enableTime: true, 
-    // Enables time picker
     time_24hr: true,
-    // Displays time picker in 24 hour mode without AM/PM selection when enabled.
     defaultDate: new Date(),
-    // Sets the initial selected date(s).
     minuteIncrement: 1,
-    // Adjusts the step for the minute input (incl. scrolling)
 
     onClose(selectedDates) {
-        // Function(s) to trigger on every time the calendar is closed. See Events API
-        const defaultDate = new Date();
-        const delta = selectedDates[0] - defaultDate;
-        console.log(delta);
-        if (selectedDates[0] < defaultDate) {
-            alert("Please choose a date in the future");
-        } else {
-            startBtn.disabled = false;
-        }
-     
-        const result = convertMs(delta);
-       
-        daysRef.textContent = (String(result.days)).padStart(2, "0");
-        hoursRef.textContent = (String(result.hours)).padStart(2, "0");
-        minutesRef.textContent = (String(result.minutes)).padStart(2, "0");
-        secondsRef.textContent = (String(result.seconds)).padStart(2, "0");
-
-        // setInterval(convertMs, 1000);
+        let chosenDate = selectedDates[0].getTime();
+        checkDate(chosenDate, todaysDate)
     },
 
 };
+function checkDate(date1, date2) {
+    const delta = date1 - date2;
+    const result = convertMs(delta);
+ 
+    if (date1 < date2) {
+        alert("Please choose a date in the future");
+    } else {
+        startBtn.disabled = false;
 
+        startBtn.addEventListener("click", () => {
+            // timerId = setInterval(() => {
+            //     daysRef.textContent = (String(result.days)).padStart(2, "0");
+            //     hoursRef.textContent = (String(result.hours)).padStart(2, "0");
+            //     minutesRef.textContent = (String(result.minutes)).padStart(2, "0");
+            //     secondsRef.textContent = (String(result.seconds)).padStart(2, "0");
+            // }, 1000);
+
+            if (timerId > 0) {
+                return;
+              }
+              timerId = setInterval(() => {
+
+                const { days, hours, minutes, seconds } = result;
+            
+                if (delta >= 0) {
+                  daysRef.textContent = days.toString().padStart(2, "0");
+                  hoursRef.textContent = hours.toString().padStart(2, "0");
+                  minutesRef.textContent = minutes.toString().padStart(2, "0");
+                  secondsRef.textContent = seconds.toString().padStart(2, "0");
+                } else {
+                    clearInterval(timerId);
+                    startBtn.disabled = true;
+                  }
+              }, 1000);
+
+        });
+       
+    }
+}
 
   
 flatpickr(pickerInput, options);
 
+
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
@@ -57,16 +80,11 @@ function convertMs(ms) {
   
     
     const days = Math.floor(ms / day);
-    
     const hours = Math.floor((ms % day) / hour);
-    
     const minutes = Math.floor(((ms % day) % hour) / minute);
-
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   
     return { days, hours, minutes, seconds };
-
 }
-
 
 
